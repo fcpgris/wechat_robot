0c54d4795778868c5cb8bd40734e257429ab885d
xinyuan <xinyuan.zl@alibaba-inc.com>
2015-06-27 23:24:21 +0800

    intend to add an compile error

--- src/main/java/com/ericzh/WechatServiceLauncher.java
+++ src/main/java/com/ericzh/WechatServiceLauncher.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Hello Eric!
  *
  */
-@SpringBootApplication
+@SpringBootApplication111
 public class WechatServiceLauncher 
 {
 	//TODO check log level
